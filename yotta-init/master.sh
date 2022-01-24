2b08292944e2aed2e380fa3786bfaabd4afcb594
RamSailopal <thelinuxguys@yahoo.com>
2022-01-24 16:26:57 +0000

    Issue with the repl file

--- yotta-init/master.sh
+++ yotta-init/master.sh
@@ -4,3 +4,4 @@ source /opt/yottadb/current/ydb_env_set
 /opt/yottadb/current/mupip set -replication=on -region "*"
 /opt/yottadb/current/mupip replicate -instance_create -name=instA
 /opt/yottadb/current/mupip replicate -source -start -instsecondary=instB -secondary=instB:4001 -buffsize=1048576 -log=/root/A_B.log
+cp /data/yottadb.repl /data/r1.32_x86_64/g
